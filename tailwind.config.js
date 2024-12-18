5af9457737187a22bd8e782f2940603d77a2410e
marcooo <129814665+izmarcoe@users.noreply.github.com>
2024-12-18 23:30:13 +0800

    Add Tailwind CSS integration and update npmignore files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,php,js}",
+    "./features/*.{html,php,js}", 
+    "./php/*.{html,php,js}", 
+    "./js/*.{html,php,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
