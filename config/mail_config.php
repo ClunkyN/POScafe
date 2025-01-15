4261747d022b5ed6e4fa05657903587edb796daa
marcooo <129814665+izmarcoe@users.noreply.github.com>
2025-01-15 16:33:05 +0800

    ADDED OTP FOR FORGOT PASSWORD

---
+++ config/mail_config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_username' => 'marcobarraquio091504@gmail.com', // Replace with your Gmail
+    'smtp_password' => 'suyq qecg vnmt fysi', // Replace with Gmail App Password
+    'smtp_secure' => 'tls',
+    'smtp_port' => 587,
+    'from_name' => 'POS Cafe System'
+];
