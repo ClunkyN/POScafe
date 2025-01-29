6f30f8fbb412aa10f67e06091a0c50b490b2a7ff
marcooo <129814665+izmarcoe@users.noreply.github.com>
2025-01-29 16:08:27 +0800

    Remove profile link from dropdown menu in topbar

--- features/component/topbar.php
+++ features/component/topbar.php
@@ -48,7 +48,6 @@ try {
 
         <!-- Dropdown Menu -->
         <div id="dropdownMenu" class="hidden absolute right-0 mt-2 w-48 bg-[#F2DBBE] rounded-md shadow-lg py-1">
-            <a href="profile.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
             <a href="settings.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
             <hr class="my-1">
             <?php if ($_SESSION['role'] === 'admin'): ?>
